dd211ce2771b58373d5210cf281d60dab5d0d632
David Samerson <samerson10@gmail.com>
2018-01-22 15:30:33 -0300

    Operadores

---
+++ Curso JavaScript - 01/Operadores/js/script.js
@@ -0,0 +1,12 @@
+/* 
+ * Função Estudo dos operadores JavaScript
+ */
+
+ var x = 9 + 4; //Soma
+ alert(x);
+
+ var x = 10 / "4"; //converte a string em inteiro
+ alert(x);
+
+ var x = 9 + "4"; //concatena
+ alert(x);
