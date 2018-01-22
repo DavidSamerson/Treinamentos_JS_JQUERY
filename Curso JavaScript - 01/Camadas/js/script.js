dd7f6110d3472bcf3c39d5929753084b8a289f8e
David Samerson <samerson10@gmail.com>
2018-01-22 14:35:43 -0300

    novos Commits

---
+++ Curso JavaScript - 01/Camadas/js/script.js
@@ -0,0 +1,20 @@
+function ligar() {
+
+    var nome = "David Samerson";
+    var nomeComAspas = "David\"Samerson";
+    var nomeComBarra = "David\\Samerson";
+    var nomeUnicode = "David Samerson \U+009F";
+    var nomeComQuebraDeLinha = "David Samerson \n";
+
+    var indefinida;
+    var nulo = null;
+    var nome2 = 3 * "Pessoas"; //NaN
+    
+
+    var idade = 21;
+    var lindo = true;
+
+
+    
+ window.document.getElementById("xti").src="manicure2.png";
+}
