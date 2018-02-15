68b10e6fb859ad20456697008d02b0ba4b68c536
David Samerson <samerson10@gmail.com>
2018-02-15 14:27:00 -0300

    objeto number

--- Curso JavaScript - 01/Objetos com js/js.js
+++ Curso JavaScript - 01/Objetos com js/js.js
@@ -3,6 +3,30 @@ alert(nome);
 alert(typeof(nome));
 alert(nome.length);
 
+////////////////////////////////////////////
+//Boolean
+
 var b = new Boolean (true);
+var b = new Boolean (0);
+var b = new Boolean (null);
+var b = new Boolean (NaN);
+// todas são falses
+
 alert(b);
 alert(typeof(b));
+
+/////////////////////////////////////////
+//Number
+
+var s =  new Number(120.90);
+var s =  new Number("120.90");
+
+alert(Number.NaN);
+alert(Number.MAX_VALUE);
+alert(Number.MIN_VALUE);
+alert(Number.NEGATIVE_INFINITY);
+alert(Number.POSITIVE_INFINITY);
+
+alert (s.toPrecision());// notação cientifica caso o numero citado seja menor que o numero de casas.
+alert(s.toExponential()); // notação cientifica obrigatoriamente baseado no numero de casas.
+alert(s.toFixed()); // numero de casas depois da virgula.
