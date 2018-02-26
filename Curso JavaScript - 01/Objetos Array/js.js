847a4d619b0d4ac19475d5a6cc09d05b73679c5c
David Samerson <samerson10@gmail.com>
2018-02-26 16:14:27 -0300

    Arrays js

--- Curso JavaScript - 01/Objetos Array/js.js
+++ Curso JavaScript - 01/Objetos Array/js.js
@@ -24,3 +24,35 @@ alert( filas.push("G") ); // Adiciona um novo elemento
 alert( filas.shift() ); //Exclui o primeiro elemento da fila
 alert( filas.pop()); //Remove o ultimo elemento da fila
 alert( filas.unshift("G") ); //Adiciona o elemento especificado no inicio da fila.
+
+// Multidimencional [][]
+// var pessoas = ["Ricardo" , "sandra"];
+
+var pessoas = new Array();
+pessoas[0] = new Array("Ricardo","M");
+
+alert(pessoas[0][0]);
+
+var pessoas = [
+                ["Ricardo", "M"],
+                ["Sandra","F" ]
+            ];
+
+
+
+//Arrays Associativos
+
+var pessoas = {nome:"David Samerson",
+                idade:"20",
+                endereco:"Rua 1"};
+    
+    alert(pessoas.nome);
+
+//Funções JS
+
+ alert(pessoas.every(funcao)); // verifica se todos os elementos do array satisfaz a função
+ alert(pessoas.some(funcao)); // verifica se pelo menos um elemento satisfaz a função
+ alert(pessoas.filter(funcao)); //filtra e me retorna todos os elementos que a função aceitar
+ alert(pessoas.map(funcao)); //mapeia todos os elementos do jeito que a função especifica
+
+    
