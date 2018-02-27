92daed76d8de32a98e1deeaa599c6a1b5bd650c4
David Samerson <samerson10@gmail.com>
2018-02-27 15:32:24 -0300

    poltrona

---
+++ Curso JavaScript - 01/objeto array selecionador de poltrona/codigo.js
@@ -0,0 +1,13 @@
+
+var poltronas = [false, true, false, true, true, true, false, true, false];
+
+function carregarProgramas(){
+
+    var imagens = getElementByTagName("img");
+
+    for (var i = 0; i < imagens.length; i++){
+
+        imagens[i].src = "poltrona_disponivel.png";
+        
+    }
+}
