4723494887b395285b61659030668ebdfaa87092
David Samerson <samerson10@gmail.com>
2018-03-13 14:31:33 -0300

    código poltronas

--- Curso JavaScript - 01/objeto array selecionador de poltrona/codigo.js
+++ Curso JavaScript - 01/objeto array selecionador de poltrona/codigo.js
@@ -17,3 +17,46 @@ function carregarPoltronas(){
         }
     }
 }
+
+function selecionarPoltrona(){
+
+    var imagens = document.getElementsByTagName("img");
+
+    for(var i=0 ; i<poltronas.length ; i++){
+        if(poltronas[i]){
+            imagens[i].src = "poltrona_selecionada.png";
+
+            var quer = confirm("Deseja finalizar a seleção?");
+
+            if (quer){
+                break;
+            }
+
+            imagens[i].src = "poltrona_disponivel.png";
+        }
+    }
+}
+
+function selecionaCasal() {
+
+    var imagens = document.getElementsByTagName("img");
+
+    for(var i=0 ; i<poltronas.length ; i++){
+
+        if(poltronas[i] && poltronas[i+1]){
+
+            imagens[i].src = "poltrona_selecionada.png";
+            imagens[i + 1].src = "poltrona_selecionada.png";
+
+            var quer = confirm("Deseja finalizar a seleção?");
+
+            if (quer){
+                break;
+            } else {
+                imagens[i].src = "poltrona_disponivel.png";
+                imagens[i + 1].src = "poltrona_disponivel.png";
+            }
+        }
+    }
+
+}
